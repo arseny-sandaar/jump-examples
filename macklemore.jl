7c763b8f04f9665afe14b96376fe725aeab4a736
arseny-sandaar <70660112+arseny-sandaar@users.noreply.github.com>
2020-12-25 00:19:11 +0300

    Update macklemore.jl
    Thanks for the example.
    It didn't work for me, though I changed in my version the code a bit
    
    `CbcSolver` is no longer supported. If you are using JuMP, upgrade to the latest version and use `Cbc.Optimizer` instead. If you are using MathProgBase (e.g., via `lingprog`), you will need to upgrade to MathOptInterface (https://github.com/jump-dev/MathOptInterface.jl).
    in top-level scope at macklemore.jl:11
    in CbcSolver at Cbc\8ANWi\src\Cbc.jl:36
    in #CbcSolver#11 at Cbc\8ANWi\src\Cbc.jl:36
    in error at base\error.jl:33

--- macklemore.jl
+++ macklemore.jl
@@ -1,36 +1,34 @@
 #=
 We are going to the thrift store and need 99 cents. What is the least amount of
 weight we need to carry?
-
 i.e. a knapsack problem
-
 We specify that you need at least 99 cents - does the answer change if you need exact change?
 =#
 
 using JuMP
 using Cbc # Open source solver. Must support integer programming.
 
-m = Model(solver=CbcSolver())
+m = Model(Cbc.Optimizer)
 
 # Variables represent how many of each coin we want to carry
-@defVar(m, pennies >= 0, Int)
-@defVar(m, nickels >= 0, Int)
-@defVar(m, dimes >= 0, Int)
-@defVar(m, quarters >= 0, Int)
+@variable(m, pennies >= 0, Int)
+@variable(m, nickels >= 0, Int)
+@variable(m, dimes >= 0, Int)
+@variable(m, quarters >= 0, Int)
 
 # We need at least 99 cents
-@addConstraint(m, 1 * pennies + 5 * nickels + 10 * dimes + 25 * quarters >= 99)
+@constraint(m, 1 * pennies + 5 * nickels + 10 * dimes + 25 * quarters >= 99)
 
 # Minimize mass (Grams)
 # (source: US Mint)
-@setObjective(m, Min, 2.5 * pennies + 5 * nickels + 2.268 * dimes + 5.670 * quarters)
+@objective(m, Min, 2.5 * pennies + 5 * nickels + 2.268 * dimes + 5.670 * quarters)
 
 # Solve
-status = solve(m)
+status = optimize!(m)
 
-println("Minimum weight: ", getObjectiveValue(m), " grams")
+println("Minimum weight: ", objective_value(m), " grams")
 println("using:")
-println(round(getValue(pennies)), " pennies") # "round" to cast as integer
-println(round(getValue(nickels)), " nickels")
-println(round(getValue(dimes)), " dimes")
-println(round(getValue(quarters)), " quarters")
+println(round(value(pennies)), " pennies") # "round" to cast as integer
+println(round(value(nickels)), " nickels")
+println(round(value(dimes)), " dimes")
+println(round(value(quarters)), " quarters")
